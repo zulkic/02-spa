01a552729dbd3aa9a249046fc70f9155fa58d6b3
Francisco Moya <f.moya.gonzalez@accenture.com>
2018-08-15 13:35:34 -0300

    modificando form component

--- src/app/components/form/form.component.ts
+++ src/app/components/form/form.component.ts
@@ -12,31 +12,31 @@ export class FormComponent implements OnInit {
   ngOnInit() {
   }
 
-  validaRut(e) {
-    console.log('primer if', e.target.value.length <= 2, !/[0-9]/.test(e.target.value));
-    console.log(e.target.value);
-    if (!/^0*(\d{0,3}(\.?\d{3})*)\-?([\dkK])$/.test(e.target.value)) {
-      console.log("entro aca");
-      console.log(e.target.value);
-      e.target.value = e.target.value.slice(0, -1);
-      console.log(e.target.value);
-    }
-
- /*    validaRut(e) {
+  /*   validaRut(e) {
       console.log('primer if', e.target.value.length <= 2, !/[0-9]/.test(e.target.value));
       console.log(e.target.value);
-      if (e.target.value.length === 1) {
-        if (!/[1-9]/.test(e.target.value)) {
-          console.log(e.target.value);
-          e.target.value = '';
-          console.log(e.target.value);
-        }
-      } else if (!/^0*(\d{1,3}(\.?\d{3})*)\-?([\dkK])$/.test(e.target.value)) {
+      if (!/^0*(\d{0,3}(\.?\d{3})*)\-?([\dkK])$/.test(e.target.value)) {
         console.log("entro aca");
         console.log(e.target.value);
         e.target.value = e.target.value.slice(0, -1);
         console.log(e.target.value);
       } */
+
+  validaRut(e) {
+    console.log('primer if', e.target.value.length <= 2, !/[0-9]/.test(e.target.value));
+    console.log(e.target.value);
+    if (e.target.value.length === 1) {
+      if (!/[1-9]/.test(e.target.value)) {
+        console.log(e.target.value);
+        e.target.value = '';
+        console.log(e.target.value);
+      }
+    } else if (!/^0*(\d{1,3}(\.?\d{3})*)\-?([\dkK])$/.test(e.target.value)) {
+      console.log("entro aca");
+      console.log(e.target.value);
+      e.target.value = e.target.value.slice(0, -1);
+      console.log(e.target.value);
+    }
 
     /*     validaRut(e) {
           console.log('primer if',e.target.value.length <= 2, !/[0-9]/.test(e.target.value) );
