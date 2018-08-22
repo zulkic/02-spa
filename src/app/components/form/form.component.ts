9db2bf687fae4c81c95ffc61c44b22659a41c86c
Francisco Moya <fmoyagon@gmail.com>
2018-08-22 00:44:27 -0300

    jugoseando con los formularios

--- src/app/components/form/form.component.ts
+++ src/app/components/form/form.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-form',
@@ -6,6 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./form.component.css']
 })
 export class FormComponent implements OnInit {
+
+  @ViewChild('coordenate1') coordenada1;
+  @ViewChild('coordenate2') coordenada2;
+  @ViewChild('coordenate3') coordenada3;
+  @ViewChild('coordenate4') coordenada4;
+  @ViewChild('coordenate5') coordenada5;
+  @ViewChild('coordenate6') coordenada6;
+  @ViewChild('coordenate7') coordenada7;
+  @ViewChild('coordenate8') coordenada8;
+  public button1: boolean = true;
 
   constructor() { }
 
@@ -57,5 +67,40 @@ export class FormComponent implements OnInit {
     /*     if (e.keyCode === 69) {
     
         } */
+  }
+
+  focusNext(index) {
+    if (index != 4 ) {
+      console.log('el index es', index);
+      this[`coordenada${Number(index) + 1}`].nativeElement.focus();
+    }
+    console.log(this[`coordenada${index}`].nativeElement.value);
+    if (this.coordenada1.nativeElement.value && this.coordenada2.nativeElement.value && this.coordenada3.nativeElement.value && this.coordenada4.nativeElement.value) {
+      this.validateOptions();
+    }
+  }
+
+  focusStep(index) {
+    if (index != 8 ) {
+      console.log('el index es', index);
+      this[`coordenada${Number(index) + 1}`].nativeElement.focus();
+    }
+    console.log(this[`coordenada${index}`].nativeElement.value);
+  }
+
+  validateOptions() {
+    this.button1 = false;
+    this.coordenada5.nativeElement.focus();
+    console.log('valido opciones', this.coordenada1.nativeElement.value, this.coordenada2.nativeElement.value, this.coordenada3.nativeElement.value, this.coordenada4.nativeElement.value);
+
+  }
+
+  clearValue(index) {
+    this[`coordenada${index}`].nativeElement.value = null;
+    console.log(this[`coordenada${index}`].nativeElement.value);
+  }
+
+  recoverInfo() {
+
   }
 }
